c6880fe4e1355fe3b23d4bf29ad651f0878f5b07
programming.nf <programming.nf@gmail.com>
2025-01-12 21:49:13 +0000

    remove linux

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,4 +1,4 @@
-const signUpHeaderText = 'لطفا اطلاعات خود را برای ثبت‌ نام وارد کنید.';
-const loginHeaderText = 'لطفا اطلاعات خود را برای ورود وارد کنید.';
-const alreadyHaveAccount = 'قبلا حساب کاربری دارید؟';
-const dontHaveAccount = 'حساب کاربری ندارید؟';
+const String signUpHeaderText = 'لطفا اطلاعات خود را برای ثبت‌ نام وارد کنید.';
+const String loginHeaderText = 'لطفا اطلاعات خود را برای ورود وارد کنید.';
+const String alreadyHaveAccount = 'قبلا حساب کاربری دارید؟';
+const String dontHaveAccount = 'حساب کاربری ندارید؟';
